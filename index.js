05104369473abc67c94fbd481d98f1e00db4835c
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-11-02 17:09:00 +0530

    removed admin routes, added origin to cors

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const userRoutes = require("./routes/user.routes");
-const adminRoutes = require("./routes/admin.routes");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000/",
+  })
+);
 const PORT = 5000;
 
 mongoose.connect("mongodb://localhost:27017/mgmtDB", {
@@ -24,7 +27,6 @@ database.once("open", () => {
 });
 
 app.use("/users", userRoutes);
-app.use("/admin", adminRoutes);
 
 app.get("/", (req, res) => {
   res.send("Home Route");
